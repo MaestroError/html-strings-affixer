00544d87b55773bba8790c13deff638cf71ca38f
maestroerror <revaz.gh@gmail.com>
2022-07-16 04:01:57 +0400

    remove affixer

--- affixer/backup.go
+++
@@ -1,9 +0,0 @@
-package affixer
-
-type Backup struct {
-	backup_folder string
-}
-
-/*
-	@todo add backup_folder in configs
-*/
--- affixer/logger.go
+++
@@ -1,9 +0,0 @@
-package affixer
-
-type Logger struct {
-	log_folder string
-}
-
-/*
-	@todo add log_folder in configs
-*/
