d2189acc3b0af6c0622cecfc2b74b9d59803f7e4
maestroerror <revaz.gh@gmail.com>
2022-07-22 15:20:22 +0400

    pages-test updated with fresh file

--- testdata/pages-test/test.blade.php
+++ testdata/pages-test/test.blade.php
@@ -4,44 +4,77 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ __('Document Title') }}</title>
+    <title>Document Title</title>
 </head>
 <body>
-    <p>{{ __('Testing string') }}</p>
-    <p>{{ __('Testing number 1245') }}</p>
+
+    <!-- Simple string test -->
+    <p>Testing string</p>
+    <p>Testing number 1245</p>
     
     <!-- Some ignoring characters test -->
     <p>Testing not found {} () string</p>
     <p>#hastag double #</p>
     <p>% Percent %</p>
     <p>_underscore _</p>
-    <p>{{ __('Testing dash - string') }}</p>
-    <p>{{ __('Testing non-lating სატესტო') }}</p>
+    <p>Testing 'single' quotes</p>
+    <p>Testing single 'quotes'</p>
+    <input placeholder="Some text with 'quotes'" />
+    <!-- XX Cant find last single quoted word - SOLVED! -->
+    <p>#John with 'single'</p>
 
 
-    <input placeholder="{{ __('Enter some text') }}" />
-    <!-- case insensitive and single-quotes -->
-    <input Placeholder='{{ __('Some text 2') }}' />
-    <input Placeholder='Placeholder' />
+    <!-- Other tests -->
+    <p>Testing dash - string</p>
+    <p>Testing non-latin სატესტო</p>
+
+
+    <!-- Placeholder test +case insensitive and single-quotes -->
+    <input placeholder="Enter some text" />
+    <input Placeholder='Some text 2' />
+
+    <!-- XX replaces first placeholder  - SOLVED! (passing) -->
+    <input Placeholder='test Placeholder' />
     <input placeholder="placeholder" />
 
-    <img alt="{{ __('Alt text for image') }}" />
+
+    <!-- other tests -->
+    <img alt="Alt text for image" />
 
     <!-- Hashtag extraction -->
-    <input type="submit" value="{{ __('Send') }}" />
-    <input type="submit" value=" {{ __('value') }}" />
-    <input type="submit" value=" {{ __(' Recived') }}" />
-    <p>{{ __('John with double') }}</p>
-    <!-- XX it ignores this string because of duplicate check -->
-    <p>{{ __('John with double quotes') }}</p>
+    <input type="submit" value="#Send" />
+    <input type="submit" value=" #value" />
+    <input type="submit" value=" # Recived" />
+    <!-- XX it ignores this string because of duplicate check - SOLVED! -->
+    <p>#John with double quotes</p>
 
-    <!-- Title extraction -->
-    <p title='{{ __('John "ShotGun" Nelson') }}'>{{ __('John with double quotes') }}</p>
+    <!-- Title extraction (with double quotes) -->
+    <p title='John "ShotGun" Nelson'>John with double quotes</p>
 
-    <!-- XX It removes quotes in middle of string (Shouldn't) -->
-    <p title='{{ __('John "ShotGun" Nelson') }}'>{{ __('John with double quotes') }}</p>
-    <p>{{ __('John with "double"') }}</p>
-    <input Placeholder='{{ __('Some "text" - 2') }}' />
+    <!-- XX It removes quotes in middle of string (Shouldn't) - SOLVED! -->
+    <p title='John "ShotGun" Nelson'>John with "double" quotes</p>
+
+    <!-- XX Didn't removes hashtag after replace - SOLVED! -->
+    <p>#John with "double"</p>
+
+    <input Placeholder='Some "text" - 2' />
+
+    <!-- XX Didn't finds with regex - SOLVED! (new regex) -->
+    <li>Up to 5 users simultaneously</li>
     
+    <!-- XX Didn't finds with regex - SOLVED! (new regex) -->
+    <div class="ps-product__badge">
+        <div class="ps-badge ps-badge--hot">Hot</div>
+    </div>
+
+    <!-- XX Didn't finds with regex if starting or ending with space - SOLVED! (With 3rd version of regex) -->
+    <li> Up to 5 users simultaneously </li>
+
+    <!-- No extra empty strings and mutching linebreaks with new text type suffix +TrimSpaces -->
+    <li>
+        Up to 5 users simultaneously
+    </li>
+
+
 </body>
 </html>
